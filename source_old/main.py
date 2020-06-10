96348cfbab87fa359679521b0be3adbad63f87d1
gincheong <gincheong2@gmail.com>
2020-06-10 11:58:53 +0900

    remove old files AWS에 원본 파일 있기도 하고, 어차피 Git에도 기록 남으니까 그냥 삭제하자

--- source_old/main.py
+++
@@ -1,9 +0,0 @@
-import keyNoti
-
-if __name__ == "__main__":
-
-  bot_token = <BOT_TOKEN>
-  serviceAccountKey = <PATH/TO/KEY>
-  firebaseURL = <FIREBASE_URL>
-
-  keyNoti.keyNotiBot(bot_token, serviceAccountKey, firebaseURL).boot()
