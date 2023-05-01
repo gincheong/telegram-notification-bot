d88338c8a47386dcbd5b342df316015746389db2
gincheong2 <gincheong2@gmail.com>
2023-05-01 16:47:49 +0900

    feat(env): 환경변수 미 등록 시 에러 처리하는 함수 추가

--- typescript/src/config/env.ts
+++ typescript/src/config/env.ts
@@ -1,15 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const getEnvironmentVariable = (envName: string) => {
+  const environmentVariable = process.env[envName];
+
+  if (environmentVariable === undefined) {
+    throw new Error(`Exception while reading environment variable from '.env' file: ${envName}`);
+  }
+
+  return environmentVariable;
+};
+
 export const EnvironmentVariables = {
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN as string,
-  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY as string,
-  FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL as string,
-  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN as string,
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID as string,
-  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET as string,
-  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
-  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID as string,
-  FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID as string,
-  FIREBASE_SERVICE_ACCOUNT_KEY_PATH: process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH as string,
+  TELEGRAM_BOT_TOKEN: getEnvironmentVariable('TELEGRAM_BOT_TOKEN'),
+  FIREBASE_API_KEY: getEnvironmentVariable('FIREBASE_API_KEY'),
+  FIREBASE_DATABASE_URL: getEnvironmentVariable('FIREBASE_DATABASE_URL'),
+  FIREBASE_AUTH_DOMAIN: getEnvironmentVariable('FIREBASE_AUTH_DOMAIN'),
+  FIREBASE_PROJECT_ID: getEnvironmentVariable('FIREBASE_PROJECT_ID'),
+  FIREBASE_STORAGE_BUCKET: getEnvironmentVariable('FIREBASE_STORAGE_BUCKET'),
+  FIREBASE_MESSAGING_SENDER_ID: getEnvironmentVariable('FIREBASE_MESSAGING_SENDER_ID'),
+  FIREBASE_APP_ID: getEnvironmentVariable('FIREBASE_APP_ID'),
+  FIREBASE_MEASUREMENT_ID: getEnvironmentVariable('FIREBASE_MEASUREMENT_ID'),
+  FIREBASE_SERVICE_ACCOUNT_KEY_PATH: getEnvironmentVariable('FIREBASE_SERVICE_ACCOUNT_KEY_PATH'),
 };
